81395c8ee038c5229803b366844bfbb911cce79f
henry-150 <dsharsh150@gmail.com>
2026-04-07 18:39:27 +0530

    prod bug solved

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SessionWrapper } from "@/components/SessionWrapper";
 import PageTransition from "@/components/PageTransition";
-import './bones/registry'
+import '../bones/registry'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
